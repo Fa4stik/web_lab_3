d9f41dae1dd1ec0b45c23cfdc226283a9654edbe
Stepan <stepan.sharifulin@mail.ru>
2023-10-07 17:05:33 +0500

    Next commit

--- click.php
+++ click.php
@@ -3,7 +3,7 @@ if (isset($_GET['banner'])) {
     $bannerClicked = $_GET['banner'];
     $fileCounter = "counter.txt";
     $currentData = file_get_contents($fileCounter);
-    $currentData .= "Клик по баннеру: " . $bannerClicked . "\n";
+    $currentData .= "Клик по баннеру: 0" . $bannerClicked . ".gif\n";
     file_put_contents($fileCounter, $currentData);
 
     // Перенаправление на нужную страницу
